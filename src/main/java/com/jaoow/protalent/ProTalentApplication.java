1d79557c599d5132241ee457bca6b1735420c565
Jaoow <xfx.joao@gmail.com>
2024-11-01 19:31:53 -0300

    Initial commit

---
+++ src/main/java/com/jaoow/protalent/ProTalentApplication.java
@@ -0,0 +1,13 @@
+package com.jaoow.protalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProTalentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProTalentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaoow/protalent/ProTalentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaoow.protalent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProTalentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
